0e95ec711fe5f7f2c904f2f03f6162e2c300dd69
denis nash <nasheba95@gmail.com>
2023-01-17 22:19:33 +0200

    final

--- script.js
+++ script.js
@@ -41,15 +41,39 @@ function documentActions(e) {
 			tabItems[getIndex(tabNavItem)].classList.add('active');
 		}
 	}
+	/*up*/
+	if(targetElement.closest('.footer_up')){
+		window.scrollTo({
+			top:0,
+			behavior:"smooth"
+		});
+		e.preventDefault();
+	}
 }
 
 function getIndex(el){
 	return Array.from(el.parentNode.children).indexOf(el);
 }
 
+const items = document.querySelectorAll('[data-item]');
+const options = {
+	threshold: 0.2
 }
 
+const callback = (entries) => {
+	entries.forEach(entry => {
+		if(entry.isIntersecting) {
+			entry.target.classList.add('active');
+		} 
+	});
+}
+const observer = new IntersectionObserver(callback, options);
 
-/*........*/
+items.forEach(item => {
+	observer.observe(item);
+});
+
+}
+
 
 
